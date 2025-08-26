5c3b3f79e0e8e5d2f37ca647bb48f23286c90ce8
Josie Barber <barbe497@umn.edu>
2025-08-26 11:20:16 -0500

    Add comments
    Added comments to all relevant lines to explain what they are doing.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-int=$1
-tar -xzf NthPrime.tgz
+int=$1 #sets int to the first input
+tar -xzf NthPrime.tgz #extracts the NthPrime tar archive
 
-cd NthPrime || exit
-gcc -o NthPrime nth_prime.c main.c
+cd NthPrime || exit #moves to the NthPrime dir, if fails, exit
+gcc -o NthPrime nth_prime.c main.c #compiles the c files
 
-./NthPrime $1
+./NthPrime $1 #runs NthPrime with the given parameter
