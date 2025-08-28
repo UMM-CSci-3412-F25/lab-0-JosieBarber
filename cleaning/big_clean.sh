4232ea24323a94d9446826bfe242a00864725b1d
Josie Barber <barbe497@umn.edu>
2025-08-28 11:35:26 -0500

    Update variable names + Comments
    This commit also finishes out the lab

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
+DIRECTORY=$1
+BASE_NAME=$(basename "$DIRECTORY" .tgz)
+HERE=$(pwd)
+SCRATCH=$(mktemp --directory SCRATCH-XXXXXX)
+CLEAN="cleaned_$BASE_NAME.tgz"
 
-LITTLE=little_dir.tgz
-BIG=big_dir.tgz
+tar -xzf "$DIRECTORY" -C "$SCRATCH" #extracts the little_dir tar archive
+cd "$SCRATCH" || exit #moves to scratch dir
+grep -lrIZ 'DELETE ME' . | xargs -0 rm -f #removes files that say "DELETE ME!"
+tar -czf "$CLEAN" "$BASE_NAME" #compresses the contents of the scratch dir
+mv "$CLEAN" "$HERE" #move comrpessed contents to "cleaning" dir
+cd "$HERE" || exit
+rm -r "$SCRATCH" # removes scratch dir
 
-SCRATCH_BIG=$(mktemp --directory SCRATCH_big-XXXXXX)
-SCRATCH_LITTLE=$(mktemp --directory SCRATCH_little-XXXXXX)
 
-tar -xzf $LITTLE -C  $SCRATCH_LITTLE #extracts the little_dir tar archive
-tar -xzf $BIG -C  $SCRATCH_BIG #extracts the big_dir tar archive
+
