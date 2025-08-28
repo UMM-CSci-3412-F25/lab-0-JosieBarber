afe0ccc0c3aa9c34b4d3d60d83e1f71317a79086
Josie Barber <barbe497@umn.edu>
2025-08-28 10:19:12 -0500

    Create big_clean shell script
    Adds functionality to big_clean script to extract both unsorted files out and put them in temporary directories.

---
+++ cleaning/big_clean.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+LITTLE=little_dir.tgz
+BIG=big_dir.tgz
+SCRATCH_BIG=$(mktemp --directory SCRATCH_big-XXXXXX)
+SCRATCH_LITTLE=$(mktemp --directory SCRATCH_little-XXXXXX)
+
+tar -xzf $LITTLE -C  $SCRATCH_LITTLE #extracts the little_dir tar archive
+tar -xzf $BIG -C  $SCRATCH_BIG #extracts the big_dir tar archive
