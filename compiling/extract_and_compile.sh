7a3f3127bbe4a1fdbff6946e9b127b749ffd7111
Josie Barber <barbe497@umn.edu>
2025-08-28 09:54:34 -0500

    Fix Shellcheck warnings
    Fixed Shellcheck warnings by adding quotes around "$1" on line 8, and getting rid of the variable definition on line 2.

--- compiling/extract_and_compile.sh
+++ compiling/extract_and_compile.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-int=$1 #sets int to the first input
+# $1 is n such that it will run NthPrime with n
 tar -xzf NthPrime.tgz #extracts the NthPrime tar archive
 
 cd NthPrime || exit #moves to the NthPrime dir, if fails, exit
 gcc -o NthPrime nth_prime.c main.c #compiles the c files
 
-./NthPrime $1 #runs NthPrime with the given parameter
+./NthPrime "$1" #runs NthPrime with the given parameter
